0243244be403e346fd37150fa309b3cf563fc1ad
ashiqdey <ashiqdey@gmail.com>
2022-10-01 11:23:30 +0530

    refactored

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom';
 // @mui
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
-// redux
-// import { Provider as ReduxProvider } from 'react-redux';
-// import store from './redux/store';
-
+//
 import App from './App';
 import theme from './theme';
 
@@ -15,9 +12,7 @@ import theme from './theme';
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    {/* <ReduxProvider store={store}> */}
     <App />
-    {/* </ReduxProvider> */}
   </ThemeProvider>,
   document.getElementById('root')
 );
